0072c0b6ee5dcecbfe0def919f00cd3747132393
Piyushh10 <shivnanip815@gmail.com>
2025-02-24 19:25:39 +0530

    dll

--- src/DSA/a33_DLL.java
+++ src/DSA/a33_DLL.java
@@ -2,12 +2,6 @@ package DSA;
 
 public class a33_DLL {
     private Node head;
-//    private Node tail;
-//    private int size;
-
-//    public a33_DLL(){
-//        this.size = 0;
-//    }
 
     public void insertAtFirst(int val){
         Node node = new Node(val);
@@ -17,83 +11,37 @@ public class a33_DLL {
             head.prev = node;
         }
         head = node;
-//        if(tail == null){
-//            tail = head;
-//        }
-//        size++;
     }
 
     public void insertAtEnd(int val){
-//        if (tail == null){
-//            insertAtFirst(val);
-//            return;
-//        }
-//        Node node = new Node(val);
-//        tail.next = node;
-//        tail = node;
-//        size++;
-
+        Node node = new Node(val);
+        Node last = head;
+        node.next = null;
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return;
+        }
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
     }
 
-    public void insert(int val, int index){
-        if(index == 0){
-            insertAtFirst(val);
-            return;
-        }
-        if(index == size){
-            insertAtEnd(val);
-            return;
-        }
-        Node temp = head;
+    public void insert(int after, int index){
+        Node p = find(after);
+
+
+
+        /*
+         Node temp = head;
         for (int i = 1; i < index; i++) {
             temp = temp.next;
         }
         Node node = new Node(val, temp.next);
         temp.next = node;
-    }
-
-    public int deleteFirst(){
-        int val = head.value;
-        head = head.next;
-        if(head == null){
-            tail = null;
-        }
-        size--;
-        return val;
-    }
-
-    public int deleteLast(){
-        if(size <= 1){
-            return deleteFirst();
-        }
-        Node secondLast = get(size - 2);
-        int val = tail.value;
-        tail = secondLast;
-        tail.next = null;
-        return val;
-    }
-
-    public int delete(int index){
-        if(index == 0){
-            return deleteFirst();
-        }
-        if(index == size-1){
-            return deleteLast();
-        }
-
-        Node previ = get(index-1);
-        int val = previ.next.value;
-        previ.next = previ.next.next;
-        return val;
-
-    }
-
-    public Node get(int index){
-        Node node = head;
-        for (int i = 0; i < index; i++) {
-            node = node.next;
-        }
-        return node;
+         */
     }
 
     public Node find(int value) {
@@ -107,23 +55,67 @@ public class a33_DLL {
         return null;
     }
 
-    public void display(){
-        Node temp = head;
-        Node last = null;
-        while(temp != null){
-            System.out.print(temp.value + " -> ");
-            last = temp;
-            temp = temp.next;
-        }
-        System.out.println("END");
+//    public int deleteFirst(){
+//        int val = head.value;
+//        head = head.next;
+//        if(head == null){
+//            tail = null;
+//        }
+//        size--;
+//        return val;
+//    }
+//
+//    public int deleteLast(){
+//        if(size <= 1){
+//            return deleteFirst();
+//        }
+//        Node secondLast = get(size - 2);
+//        int val = tail.value;
+//        tail = secondLast;
+//        tail.next = null;
+//        return val;
+//    }
+//
+//    public int delete(int index){
+//        if(index == 0){
+//            return deleteFirst();
+//        }
+//        if(index == size-1){
+//            return deleteLast();
+//        }
+//
+//        Node previ = get(index-1);
+//        int val = previ.next.value;
+//        previ.next = previ.next.next;
+//        return val;
+//
+//    }
+//
+//    public Node get(int index){
+//        Node node = head;
+//        for (int i = 0; i < index; i++) {
+//            node = node.next;
+//        }
+//        return node;
+//    }
 
-        //reverse display
-        while(last != null){
-            System.out.print(last.value + "->");
-            last = last.prev;
-        }
-        System.out.println("START");
-    }
+//    public void display(){
+//        Node temp = head;
+//        Node last = null;
+//        while(temp != null){
+//            System.out.print(temp.value + " -> ");
+//            last = temp;
+//            temp = temp.next;
+//        }
+//        System.out.println("END");
+//
+//        //reverse display
+//        while(last != null){
+//            System.out.print(last.value + "->");
+//            last = last.prev;
+//        }
+//        System.out.println("START");
+//    }
 
     private class Node{
         private int value;
