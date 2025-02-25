3009d3711df94c0575e64e85e7ddf88e7ca6c4a1
Piyushh10 <shivnanip815@gmail.com>
2025-02-25 21:22:04 +0530

    kar diya bhai kuch to

--- src/DSA/a34_CircularLL.java
+++ src/DSA/a34_CircularLL.java
@@ -2,4 +2,41 @@ package DSA;
 
 public class a34_CircularLL {
 
+    private Node head;
+    private Node tail;
+
+    public a34_CircularLL(Node head, Node tail) {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insert(int val){
+        Node node = new Node(val);
+        if(head == null){
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+
+    public void display(){
+
+    }
+
+    private class Node{
+        private int val;
+        private Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
