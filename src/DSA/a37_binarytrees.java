3ae6f2aa48c6ce808d83eedb67f5c83b3065b81b
Piyushh10 <shivnanip815@gmail.com>
2025-03-19 15:11:48 +0530

    tree continued....

--- src/DSA/a37_binarytrees.java
+++ src/DSA/a37_binarytrees.java
@@ -1,14 +1,65 @@
 package DSA;
 
+
+import java.util.Scanner;
+
+//its  a simple binary tree, not BST
 public class a37_binarytrees {
-    public static void main(String[] args) {
+
+    public a37_binarytrees() {
 
     }
+
+    private static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    //insert elements
+    public void populate(Scanner sc){
+        System.out.println("Enter the root node value: ");
+        int value = sc.nextInt();
+        root = new Node(value);
+        populate(sc, root);
+    }
+
+    private void populate(Scanner sc, Node node){
+        System.out.println("Do you want to enter left of "+node.value);
+        boolean left = sc.nextBoolean();
+        if(left){
+            System.out.println("Enter the value of the left of "+node.value);
+            int value = sc.nextInt();
+            node.left = new Node(value);
+            populate(sc, node.left);
+        }
+
+        System.out.println("Do you want to enter right of "+node.value);
+        boolean right = sc.nextBoolean();
+        if(right){
+            System.out.println("Enter the value of the right of "+node.value);
+            int value = sc.nextInt();
+            node.right = new Node(value);
+            populate(sc, node.right);
+        }
+
+
+
+    }
+
 }
 
 
 
+/*
 
+ */
 
 
 
