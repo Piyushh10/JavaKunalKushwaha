df9bb19180e8727cfa9d6d36c9c9593853400166
Piyushh10 <shivnanip815@gmail.com>
2025-04-12 23:29:37 +0530

    bfs of graph

---
+++ src/DSA/a43_bfs.java
@@ -0,0 +1,27 @@
+package DSA;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class a43_bfs {
+    public ArrayList<Integer> bfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj){
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean vis[] = new boolean[v];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vis[0] = true;
+
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            bfs.add(node);
+            for(Integer it : adj.get(node)){
+                if(vis[it] == false){
+                    vis[it] = true;
+                    q.add(it);
+                }
+            }
+        }
+        return bfs;
+    }
+}
